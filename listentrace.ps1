013ca85a333629d17e106e28ff6449762d850a7d
BUG IT <85834131+bug-it@users.noreply.github.com>
2026-02-12 11:12:22 -0300

    Script Listen Trace (Powershell)

--- listentrace.ps1
+++ listentrace.ps1
@@ -1,4 +1,7 @@
+
 Clear-Host
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
 $Host.UI.RawUI.WindowTitle = "ListentTrace :: Auditoria / Monitoramento"
 
 # ================= CORES =================
